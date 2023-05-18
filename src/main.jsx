31931d114f93035c9a553a47f04efc2eaed48880
Jaime Pineda <jpin730@pm.me>
2023-05-18 11:20:09 -0600

    chore: :lipstick: add bootstrap cdn

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { HooksApp } from "./HooksApp";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <HooksApp></HooksApp>
+  </React.StrictMode>
 );
