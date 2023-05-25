0b11fcd481c00db441e1055ba8ed3923c6ceb476
Jaime Pineda <jpin730@pm.me>
2023-05-25 17:17:27 -0600

    test: :white_check_mark: add tests to login page component

---
+++ test/useContext/LoginPage.test.jsx
@@ -0,0 +1,60 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+import { LoginPage, UserContext } from "../../src/useContext";
+
+// const mockUseNavigate = jest.fn();
+
+// jest.mock("react-router-dom", () => ({
+//   ...jest.requireActual("react-router-dom"),
+//   useNavigate: () => mockUseNavigate,
+// }));
+
+describe("LoginPage", () => {
+  test("should render component without username", () => {
+    const mockSetUser = jest.fn();
+
+    render(
+      <BrowserRouter>
+        <UserContext.Provider value={{ setUser: mockSetUser }}>
+          <LoginPage />
+        </UserContext.Provider>
+      </BrowserRouter>
+    );
+
+    const inputElement = screen.getByRole("textbox");
+    const loginBtn = screen.getByRole("button");
+
+    expect(inputElement.value).toBe("");
+    expect(loginBtn.disabled).toBe(true);
+  });
+
+  test("should call setUser on click login button", () => {
+    const mockSetUser = jest.fn();
+
+    render(
+      <BrowserRouter>
+        <UserContext.Provider value={{ setUser: mockSetUser }}>
+          <LoginPage />
+        </UserContext.Provider>
+      </BrowserRouter>
+    );
+
+    const inputElement = screen.getByRole("textbox");
+    const loginBtn = screen.getByRole("button");
+
+    expect(inputElement.value).toBe("");
+    expect(loginBtn.disabled).toBe(true);
+
+    fireEvent.click(loginBtn);
+
+    expect(mockSetUser).not.toBeCalled();
+
+    const testInputValue = "MockedUsername";
+    fireEvent.input(inputElement, { target: { value: testInputValue } });
+
+    fireEvent.click(loginBtn);
+
+    expect(mockSetUser).toBeCalledWith(testInputValue);
+  });
+});
